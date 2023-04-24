78d12104658ce8d7313a8802ed5a62e804c0ba70
Mateusz <zynek.mateusz@wp.pl>
2023-04-24 23:37:04 +0200

    Dodano mozliwosc wyswietlenia powiekszonego podgladu zdjec, jak i dodano przycisk do zamkniecia podgladu.

--- components/ImageView.js
+++ components/ImageView.js
@@ -1,17 +1,47 @@
-import { Image, StyleSheet } from "react-native";
-
-function ImageView(props) {
-    return (
-        <Image style={styles.image} source={{uri: props.imageUri}} />
-    );
-}
-
-export default ImageView;
-
-const styles = StyleSheet.create({
-    image: {
-        height: 100,
-        width: '24%',
-        margin: '0.5%'
+import {
+    Image,
+    StyleSheet,
+    View,
+    TouchableOpacity
+  } from "react-native";
+  import ImageInspect from "./ImageInspect";
+  import { useState } from "react";
+   
+  function ImageView(props) {
+    const [modalIsVisible, setModalIsVisible] = useState(false);
+   
+    function showImageScreenHandler() {
+      setModalIsVisible(true);
     }
-});
+   
+    function closeImageScreenHandler() {
+      setModalIsVisible(false);
+    }
+   
+    return (
+      <View style={styles.imageView}>
+        <ImageInspect
+          visible={modalIsVisible}
+          onCloseImageScreen={closeImageScreenHandler}
+          imageUri={props.imageUri} // przekazanie informacji o zdjęciu do komponentu ImageInspect
+        />
+        <TouchableOpacity onPress={showImageScreenHandler}>
+          <Image style={styles.image} source={{ uri: props.imageUri }} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+   
+  export default ImageView;
+   
+  const styles = StyleSheet.create({
+    imageView: {
+      height: 100,
+      width: "24%",
+      margin: "0.5%",
+    },
+    image: {
+      height: "100%",
+      width: "100%",
+    },
+  });
